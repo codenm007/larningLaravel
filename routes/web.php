7a439513f7b8177d3dae1d03601bf59bbfc7ddcf
codenm007 <n.majumder14@gmail.com>
2022-03-19 12:52:10 +0530

    file upload route defined

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('/upload', [FileController::class,'upload']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
